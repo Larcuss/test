5cd0247b05d4a222e6676d1a2fe2da4419e86e14
Larcuss <xxksharp@foxmail.com>
2021-06-09 10:52:30 +0800

    try1·

---
+++ src/com/day2/test1.java
@@ -0,0 +1,15 @@
+package com.day2;
+
+public class test1 {
+    public static void main(String args[])
+    {
+        System.out.println("?");
+        /*需求规格说明书：敏捷开发 PROTOTYPE
+        * 史诗=功能module 大方向
+        * 故事/任务 = 功能module的方法（describe + check-option)(use-case+use-case细化)
+        * sprint = 阶段
+        * idea->GIT
+        * 先pull再push，解决冲突*/
+    }
+
+}
